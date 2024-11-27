352d8ea578c0a804f7ade9dd7c0c3e653f3a27e6
ftshijt <ftshijt@gmail.com>
2024-11-27 01:47:07 -0600

    update install utmos

--- tools/install_utmosv2.sh
+++ tools/install_utmosv2.sh
@@ -3,7 +3,16 @@
 
 rm -rf UTMOSv2
 
-git clone https://github.com/ftshijt/UTMOSv2.git
+# Check if git-lfs is installed
+if git lfs --version >/dev/null 2>&1; then
+  echo "Git LFS is installed."
+else
+  echo "Git LFS is not installed. Please install git-lfs first."
+  echo "You may check tools/install_gitlfs.md for guidelines"
+  exit 1
+fi
+
+GIT_LFS_SKIP_SMUDGE=1 git clone https://github.com/ftshijt/UTMOSv2.git
 cd UTMOSv2
 # Prevents LFS files from being temporarily downloaded during the installation process
 GIT_LFS_SKIP_SMUDGE=1 pip install -e .
