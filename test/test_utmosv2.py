8904fe67b1502c0fd6b69fff1430d996bf6cd7d0
ftshijt <ftshijt@gmail.com>
2024-12-07 20:15:50 -0600

    add test specific modeuls

---
+++ test/test_utmosv2.py
@@ -0,0 +1,61 @@
+import logging
+import math
+import os
+
+import yaml
+
+from versa.scorer_shared import (
+    find_files,
+    list_scoring,
+    load_score_modules,
+    load_summary,
+)
+
+TEST_INFO = {"utmosv2": 3.853}
+
+
+def info_update():
+
+    # find files
+    if os.path.isdir("test/test_samples/test2"):
+        gen_files = find_files("test/test_samples/test2")
+
+    # find reference file
+    if os.path.isdir("test/test_samples/test1"):
+        gt_files = find_files("test/test_samples/test1")
+
+    logging.info("The number of utterances = %d" % len(gen_files))
+
+    with open("egs/separate_metrics/utmos2.yaml", "r", encoding="utf-8") as f:
+        score_config = yaml.full_load(f)
+
+    score_modules = load_score_modules(
+        score_config,
+        use_gt=(True if gt_files is not None else False),
+        use_gpu=False,
+    )
+
+    assert len(score_config) > 0, "no scoring function is provided"
+
+    score_info = list_scoring(
+        gen_files, score_modules, gt_files, output_file=None, io="soundfile"
+    )
+    summary = load_summary(score_info)
+    print("Summary: {}".format(load_summary(score_info)), flush=True)
+
+    for key in summary:
+        if math.isinf(TEST_INFO[key]) and math.isinf(summary[key]):
+            # for sir"
+            continue
+        # the plc mos is undeterministic
+        if abs(TEST_INFO[key] - summary[key]) > 1e-4 and key != "plcmos":
+            raise ValueError(
+                "Value issue in the test case, might be some issue in scorer {}".format(
+                    key
+                )
+            )
+    print("check successful", flush=True)
+
+
+if __name__ == "__main__":
+    info_update()
